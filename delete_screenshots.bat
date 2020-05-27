7ffbde3729869edc048ffd9506592a6e3e143339
marko-despotovic-bgd <scorpyon.83.srb@gmail.com>
2020-05-27 12:07:35 +0200

    delete screenshots on click

---
+++ delete_screenshots.bat
@@ -0,0 +1,9 @@
+Echo Deleting failure_screenshots folder...
+@RD /S /Q "C:\Users\Marko\Documents\Automation\RED\RedTestFramework\failure_screenshots"
+TIMEOUT /T 10 /NOBREAK
+
+#/s parameter will delete all files contained in the directory subfolders. However if you do not want to delete files from subfolders, remove /s parameter.
+
+# /f parameter ignores any read-only setting.
+
+# /q “quiet mode,” meaning you won’t be prompted Yes/No
